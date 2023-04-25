227e6f1ade39fc1ac984a5e0439b616950aeb582
THENISHA S <93367420+thenisha009@users.noreply.github.com>
2023-04-25 13:09:02 +0530

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,42 @@
+export const QuizData = [
+    {
+        question:"A train 125 m long passes a man, running at 5 km/hr in the same direction in which the train is going, in 10 seconds. The speed of the train is:",
+        options :["45 km/hr","50 km/hr","54 km/hr","55 km/hr"],
+        answer:2,
+    },
+    {
+        question:"A is two years older than B who is twice as old as C. If the total of the ages of A, B and C be 27, then how old is B?",
+        options:["7","8","9","10"],
+        answer:4,
+    },
+    {
+        question:"Which number should come next in the series, 48, 24, 12, ......?",
+        options:["8","6","4","2"],
+        answer:2
+    },
+    {
+       question:"In the following number series a wrong number is given. Find out the wrong number. 150, 450, 750, 1060, 1350, 1650, 1950",
+       options:["1060","150","450","1350"],
+       answer: 1
+    },
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+];
